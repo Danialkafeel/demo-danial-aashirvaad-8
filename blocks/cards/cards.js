57fa35993fe7e17467185ebf9451437ced2957d8
danialkafee <danialkafee@adobe.com>
2024-07-10 12:16:10 +0530

    Add accepted blocks from AEM Assistant

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,18 +1,15 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
-  const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
-    ul.append(li);
+  block.classList.add('custom-card-container');
+
+  const cardElements = block.querySelectorAll('.cards > div');
+  cardElements.forEach(card => {
+    card.classList.add('custom-card');
+    
+    const imgWrapper = card.querySelector('picture').parentElement;
+    imgWrapper.classList.add('custom-image-wrapper');
+    
+    const linkWrapper = card.querySelector('a').parentElement;
+    linkWrapper.classList.add('custom-link-wrapper');
   });
-  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-  block.textContent = '';
-  block.append(ul);
 }
